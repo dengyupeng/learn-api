d63b0a9bafb9d41953feb7b3a30db1a82938a017
1209885250 <1209885250@qq.com>
2018-04-02 11:36:02 +0000

    生成测试数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,11 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Lesson::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'free' => $faker->boolean(),//$faker会随机分配true/false
+    ];
+});
